b7221732e41dc1529cb0b93082cf42c454565a31
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2024-08-26 13:50:07 -0500

    Agregada la tabla y el codigo php

---
+++ actualizar.php
@@ -0,0 +1,17 @@
+<?php
+$nombre = $_GET['nombre'];
+$apellido = $_GET['apellido'];
+$correo = $_GET['correo'];
+$telefono = $_GET['telefono'];
+
+include_once "conexion.php";
+
+$conexion = conexion();
+
+$sql="UPDATE contactos SET nombre='$nombre', apellido='$apellido', correo='$correo', telefono='$telefono'
+ where telefono='$telefono'";
+$query=mysqli_query($conexion,$sql);
+if($query){
+    header("refresh:0;url=index.php");
+}
+?>
